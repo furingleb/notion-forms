3e6b606dddd732feaaaf3e6d2c10920f6e1ae6ce
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2022-10-14 15:28:42 +0530

    SCSS Removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app"
 
-import "../scss/main.scss"
+import "../index.css"
 
 const MyApp: React.FC<AppProps> = (props) => {
   const { Component, pageProps } = props
